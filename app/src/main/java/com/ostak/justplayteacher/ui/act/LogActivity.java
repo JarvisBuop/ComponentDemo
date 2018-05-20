83fc80fc2d248c6c61b2be3a9356a47f32f137f3
JarvisDong <dongmm@handsure.com>
2018-05-20 23:11:00 +0800

    Merge branch 'master' of https://gitee.com/dmmbuop/JustPlayTeacher
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/ostak/justplayteacher/ui/act/LogActivity.java
@@ -0,0 +1,35 @@
+package com.ostak.justplayteacher.ui.act;
+
+import android.os.Bundle;
+
+import com.jarvisdong.uikit.baseui.DBaseActivity;
+import com.ostak.justplayteacher.R;
+
+/**
+ * Created by JarvisDong on 2018/5/20.
+ *
+ * @Description:
+ * @see:
+ */
+
+public class LogActivity extends DBaseActivity {
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_log;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void processLogic(Bundle savedInstanceState) {
+
+    }
+}
